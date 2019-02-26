c48983c50f1d478252b05bcadf6f7b4160336881
Haspa Melodica <melodicahaspa@gmail.com>
2019-02-26 16:45:36 +0100

    Cleaned up Instruction property model

--- src/net/haspamelodica/am2900me/swtui/MicroinstructionsComposite.java
+++ src/net/haspamelodica/am2900me/swtui/MicroinstructionsComposite.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
@@ -41,91 +42,78 @@ import net.maisikoleni.am2900me.util.NBitsUInt;
 public class MicroinstructionsComposite extends Composite {
 	private static final String ERROR_NOT_IMPLEMENTED = "Not yet implemented in the SWT version...";
 
-	private final static List<InstructionEditorE<?>> instructionEnumEditors;
-	private final static List<InstructionEditorN<?>> instructionNBitsUIntEditors;
+	private final static List<InstructionPropertyEditor> instructionEditors;
 
-	private static class InstructionEditorE<E extends Enum<E>> {
-		private final Function<MicroInstruction, E> getVal;
-		private final BiFunction<MicroInstruction, E, MicroInstruction> setVal;
+	private static class InstructionPropertyEditor {
+		private final Function<MicroInstruction, String> getVal;
+		private final Predicate<String> checkVal;
+		private final BiFunction<MicroInstruction, String, MicroInstruction> setVal;
+		private final List<String> values;
 
-		private InstructionEditorE(Function<MicroInstruction, E> getVal,
-				BiFunction<MicroInstruction, E, MicroInstruction> setVal) {
+		private InstructionPropertyEditor(Function<MicroInstruction, String> getVal, Predicate<String> checkVal,
+				BiFunction<MicroInstruction, String, MicroInstruction> setVal, List<String> values) {
 			this.getVal = getVal;
+			this.checkVal = checkVal;
 			this.setVal = setVal;
+			this.values = values;
 		}
+
 	}
 
-	private static class InstructionEditorN<E extends NBitsUInt> {
-		private final Function<MicroInstruction, E> getVal;
-		private final BiFunction<MicroInstruction, E, MicroInstruction> setVal;
-		private final IntFunction<E> createVal;
+	private static <E extends Enum<E>> InstructionPropertyEditor createIPE(Function<MicroInstruction, E> getVal,
+			BiFunction<MicroInstruction, E, MicroInstruction> setVal) {
+		E defaultVal = getVal.apply(MicroInstruction.DEFAULT);
+		E[] values = defaultVal.getDeclaringClass().getEnumConstants();
+		List<String> valueNames = Arrays.stream(values).map(Object::toString).collect(Collectors.toList());
+		return new InstructionPropertyEditor(i -> getVal.apply(i).toString(), s -> valueNames.contains(s),
+				(i, s) -> setVal.apply(i, values[valueNames.indexOf(s)]), valueNames);
+	}
 
-		private InstructionEditorN(Function<MicroInstruction, E> getVal,
-				BiFunction<MicroInstruction, E, MicroInstruction> setVal, IntFunction<E> createVal) {
-			this.getVal = getVal;
-			this.setVal = setVal;
-			this.createVal = createVal;
-		}
+	private static <N extends NBitsUInt> InstructionPropertyEditor createIPE(Function<MicroInstruction, N> getVal,
+			IntFunction<N> createVal, BiFunction<MicroInstruction, N, MicroInstruction> setVal) {
+		return new InstructionPropertyEditor(i -> getVal.apply(i).toString(), s -> {
+			try {
+				Integer.decode(s);
+			} catch (NumberFormatException x) {
+				return false;
+			}
+			return true;
+		}, (i, s) -> setVal.apply(i, createVal.apply(Integer.decode(s))), null);
 	}
 
 	static {
-		ArrayList<InstructionEditorE<?>> iEsM = new ArrayList<>();
-		iEsM.add(null);
-		iEsM.add(1, new InstructionEditorE<>(MicroInstruction::getIe, MicroInstruction::withIe));
-		iEsM.add(2, null);
-		iEsM.add(3, new InstructionEditorE<>(MicroInstruction::getKmux, MicroInstruction::withKmux));
-		iEsM.add(4, null);
-		iEsM.add(5, new InstructionEditorE<>(MicroInstruction::getAm2901_Src, MicroInstruction::withAm2901_Src));
-		iEsM.add(6, new InstructionEditorE<>(MicroInstruction::getAm2901_Func, MicroInstruction::withAm2901_Func));
-		iEsM.add(7, new InstructionEditorE<>(MicroInstruction::getAm2901_Dest, MicroInstruction::withAm2901_Dest));
-		iEsM.add(8, null);
-		iEsM.add(9, new InstructionEditorE<>(MicroInstruction::getAsel, MicroInstruction::withAsel));
-		iEsM.add(10, null);
-		iEsM.add(11, new InstructionEditorE<>(MicroInstruction::getBsel, MicroInstruction::withBsel));
-		iEsM.add(12, new InstructionEditorE<>(MicroInstruction::getAbus, MicroInstruction::withAbus));
-		iEsM.add(13, new InstructionEditorE<>(MicroInstruction::getDbus, MicroInstruction::withDbus));
-		iEsM.add(14, new InstructionEditorE<>(MicroInstruction::getAm2904_Carry, MicroInstruction::withAm2904_Carry));
-		iEsM.add(15, null);
-		iEsM.add(16, new InstructionEditorE<>(MicroInstruction::getCe_mu, MicroInstruction::withCe_mu));
-		iEsM.add(17, new InstructionEditorE<>(MicroInstruction::getCe_m, MicroInstruction::withCe_m));
-		iEsM.add(18, new InstructionEditorE<>(MicroInstruction::getAm2904_Inst, MicroInstruction::withAm2904_Inst));
-		iEsM.add(19, new InstructionEditorE<>(MicroInstruction::getCcen, MicroInstruction::withCcen));
-		iEsM.add(20, new InstructionEditorE<>(MicroInstruction::getAm2910_Inst, MicroInstruction::withAm2910_Inst));
-		iEsM.add(21, null);
-		iEsM.add(22, new InstructionEditorE<>(MicroInstruction::getBz_ld, MicroInstruction::withBz_ld));
-		iEsM.add(23, new InstructionEditorE<>(MicroInstruction::getBz_ed, MicroInstruction::withBz_ed));
-		iEsM.add(24, new InstructionEditorE<>(MicroInstruction::getBz_inc, MicroInstruction::withBz_inc));
-		iEsM.add(25, new InstructionEditorE<>(MicroInstruction::getBz_ea, MicroInstruction::withBz_ea));
-		iEsM.add(26, new InstructionEditorE<>(MicroInstruction::getIr_ld, MicroInstruction::withIr_ld));
-		iEsM.add(27, new InstructionEditorE<>(MicroInstruction::getMwe, MicroInstruction::withMwe));
-		instructionEnumEditors = Collections.unmodifiableList(iEsM);
-	}
+		ArrayList<InstructionPropertyEditor> iEsM = new ArrayList<>();
+		iEsM.add(0, null);
+		iEsM.add(1, createIPE(MicroInstruction::getIe, MicroInstruction::withIe));
+		iEsM.add(2, createIPE(MicroInstruction::getInterrupt, Interrupt::new, MicroInstruction::withInterrupt));
+		iEsM.add(3, createIPE(MicroInstruction::getKmux, MicroInstruction::withKmux));
+		iEsM.add(4, createIPE(MicroInstruction::getK, Konst::new, MicroInstruction::withK));
+		iEsM.add(5, createIPE(MicroInstruction::getAm2901_Src, MicroInstruction::withAm2901_Src));
+		iEsM.add(6, createIPE(MicroInstruction::getAm2901_Func, MicroInstruction::withAm2901_Func));
+		iEsM.add(7, createIPE(MicroInstruction::getAm2901_Dest, MicroInstruction::withAm2901_Dest));
+		iEsM.add(8, createIPE(MicroInstruction::getRa_addr, RA_ADDR::new, MicroInstruction::withRa_addr));
+		iEsM.add(9, createIPE(MicroInstruction::getAsel, MicroInstruction::withAsel));
+		iEsM.add(10, createIPE(MicroInstruction::getRb_addr, RB_ADDR::new, MicroInstruction::withRb_addr));
+		iEsM.add(11, createIPE(MicroInstruction::getBsel, MicroInstruction::withBsel));
+		iEsM.add(12, createIPE(MicroInstruction::getAbus, MicroInstruction::withAbus));
+		iEsM.add(13, createIPE(MicroInstruction::getDbus, MicroInstruction::withDbus));
+		iEsM.add(14, createIPE(MicroInstruction::getAm2904_Carry, MicroInstruction::withAm2904_Carry));
+		iEsM.add(15,
+				createIPE(MicroInstruction::getAm2904_Shift, Am2904_Shift::new, MicroInstruction::withAm2904_Shift));
+		iEsM.add(16, createIPE(MicroInstruction::getCe_mu, MicroInstruction::withCe_mu));
+		iEsM.add(17, createIPE(MicroInstruction::getCe_m, MicroInstruction::withCe_m));
+		iEsM.add(18, createIPE(MicroInstruction::getAm2904_Inst, MicroInstruction::withAm2904_Inst));
+		iEsM.add(19, createIPE(MicroInstruction::getCcen, MicroInstruction::withCcen));
+		iEsM.add(20, createIPE(MicroInstruction::getAm2910_Inst, MicroInstruction::withAm2910_Inst));
+		iEsM.add(21, createIPE(MicroInstruction::getBar, BAR::new, MicroInstruction::withBar));
+		iEsM.add(22, createIPE(MicroInstruction::getBz_ld, MicroInstruction::withBz_ld));
+		iEsM.add(23, createIPE(MicroInstruction::getBz_ed, MicroInstruction::withBz_ed));
+		iEsM.add(24, createIPE(MicroInstruction::getBz_inc, MicroInstruction::withBz_inc));
+		iEsM.add(25, createIPE(MicroInstruction::getBz_ea, MicroInstruction::withBz_ea));
+		iEsM.add(26, createIPE(MicroInstruction::getIr_ld, MicroInstruction::withIr_ld));
+		iEsM.add(27, createIPE(MicroInstruction::getMwe, MicroInstruction::withMwe));
 
-	static {
-		ArrayList<InstructionEditorN<?>> iEsM = new ArrayList<>();
-		while (iEsM.size() < 2)
-			iEsM.add(null);
-		iEsM.add(2, new InstructionEditorN<>(MicroInstruction::getInterrupt, MicroInstruction::withInterrupt,
-				Interrupt::new));
-		while (iEsM.size() < 4)
-			iEsM.add(null);
-		iEsM.add(4, new InstructionEditorN<>(MicroInstruction::getK, MicroInstruction::withK, Konst::new));
-		while (iEsM.size() < 8)
-			iEsM.add(null);
-		iEsM.add(8,
-				new InstructionEditorN<>(MicroInstruction::getRa_addr, MicroInstruction::withRa_addr, RA_ADDR::new));
-		while (iEsM.size() < 10)
-			iEsM.add(null);
-		iEsM.add(10,
-				new InstructionEditorN<>(MicroInstruction::getRb_addr, MicroInstruction::withRb_addr, RB_ADDR::new));
-		while (iEsM.size() < 15)
-			iEsM.add(null);
-		iEsM.add(15, new InstructionEditorN<>(MicroInstruction::getAm2904_Shift, MicroInstruction::withAm2904_Shift,
-				Am2904_Shift::new));
-		while (iEsM.size() < 21)
-			iEsM.add(null);
-		iEsM.add(21, new InstructionEditorN<>(MicroInstruction::getBar, MicroInstruction::withBar, BAR::new));
-		instructionNBitsUIntEditors = Collections.unmodifiableList(iEsM);
+		instructionEditors = Collections.unmodifiableList(iEsM);
 	}
 
 	private final Am2900Machine machine;
@@ -239,70 +227,14 @@ public class MicroinstructionsComposite extends Composite {
 			int address = table.indexOf(item);
 			MicroInstruction instr = muProgMem.getInstruction(address);
 
-			String valAddress;
-			String valIE;
-			String valInterrupt;
-			String valKMUX;
-			String valKonst;
-			String valAm2901_Src;
-			String valAm2901_Func;
-			String valAm2901_Dest;
-			String valRA_ADDR;
-			String valASEL;
-			String valRB_ADDR;
-			String valBSEL;
-			String val_ABUS;
-			String val_DBUS;
-			String valAm2904_Carry;
-			String valAm2904_Shift;
-			String val_CE_mu;
-			String val_CE_m;
-			String valAm2904_Inst;
-			String val_CCEN;
-			String valAm2910_Inst;
-			String valBAR;
-			String val_BZ_LD;
-			String val_BZ_ED;
-			String val_BZ_INC;
-			String val_BZ_EA;
-			String val_IR_LD;
-			String val_MWE;
-
-			valAddress = HexIntStringConverter.forNibbles(3).toString(address);
-			valIE = instr.getIe().toString();
-			valInterrupt = instr.getInterrupt().toString();
-			valKMUX = instr.getKmux().toString();
-			valKonst = instr.getK().toString();
-			valAm2901_Src = instr.getAm2901_Src().toString();
-			valAm2901_Func = instr.getAm2901_Func().toString();
-			valAm2901_Dest = instr.getAm2901_Dest().toString();
-			valRA_ADDR = instr.getRa_addr().toString();
-			valASEL = instr.getAsel().toString();
-			valRB_ADDR = instr.getRb_addr().toString();
-			valBSEL = instr.getBsel().toString();
-			val_ABUS = instr.getAbus().toString();
-			val_DBUS = instr.getDbus().toString();
-			valAm2904_Carry = instr.getAm2904_Carry().toString();
-			valAm2904_Shift = instr.getAm2904_Shift().toString();
-			val_CE_mu = instr.getCe_mu().toString();
-			val_CE_m = instr.getCe_m().toString();
-			valAm2904_Inst = instr.getAm2904_Inst().toString();
-			val_CCEN = instr.getCcen().toString();
-			valAm2910_Inst = instr.getAm2910_Inst().toString();
-			valBAR = instr.getBar().toString();
-			val_BZ_LD = instr.getBz_ld().toString();
-			val_BZ_ED = instr.getBz_ed().toString();
-			val_BZ_INC = instr.getBz_inc().toString();
-			val_BZ_EA = instr.getBz_ea().toString();
-			val_IR_LD = instr.getIr_ld().toString();
-			val_MWE = instr.getMwe().toString();
-
-			item.setText(new String[] { valAddress, valIE, valInterrupt, valKMUX, valKonst, valAm2901_Src,
-					valAm2901_Func, valAm2901_Dest, valRA_ADDR, valASEL, valRB_ADDR, valBSEL, val_ABUS, val_DBUS,
-					valAm2904_Carry, valAm2904_Shift, val_CE_mu, val_CE_m, valAm2904_Inst, val_CCEN, valAm2910_Inst,
-					valBAR, val_BZ_LD, val_BZ_ED, val_BZ_INC, val_BZ_EA, val_IR_LD, val_MWE });
-
-			updateItemColor(item, address);
+			item.setText(0, HexIntStringConverter.forNibbles(3).toString(address));
+			Runnable updateItem = () -> {
+				for (int i = 1; i < 28; i++)
+					item.setText(i, instructionEditors.get(i).getVal.apply(instr));
+				updateItemColor(item, address);
+			};
+			updateItem.run();
+			machineStateChangedListenerManager.addListener(updateItem);
 		});
 		TableCursor cursor = new TableCursor(table, SWT.NONE);
 		ControlEditor editor = new ControlEditor(cursor);
@@ -315,59 +247,49 @@ public class MicroinstructionsComposite extends Composite {
 			TableItem item = cursor.getRow();
 			int address = table.indexOf(item);
 			int column = cursor.getColumn();
-			switch (column) {
-			case 0:
-				break;
-			case 2:
-			case 4:
-			case 8:
-			case 10:
-			case 15:
-			case 21:
-				MicroInstruction instr = muProgMem.getInstruction(address);
-				Text text = new Text(cursor, SWT.NONE);
-				@SuppressWarnings("unchecked")
-				InstructionEditorN<NBitsUInt> instrEditor = (InstructionEditorN<NBitsUInt>) instructionNBitsUIntEditors
-						.get(column);
-				text.setText(instrEditor.getVal.apply(instr).toString());
-				text.selectAll();
-				text.addListener(SWT.FocusOut, e2 -> {
-					try {
-						int i = Integer.decode(text.getText());
-						MicroInstruction newInstr = instrEditor.setVal.apply(instr, instrEditor.createVal.apply(i));
-						muProgMem.setInstruction(address, newInstr);
-						item.setText(column, instrEditor.getVal.apply(newInstr).toString());
-						updateItemColor(item, address);
-						machineChanged();
-					} catch (IllegalArgumentException x) {
-					}
-					text.dispose();
-				});
-				text.addListener(SWT.DefaultSelection, e2 -> {
-					try {
-						int i = Integer.decode(text.getText());
-						MicroInstruction newInstr = instrEditor.setVal.apply(instr, instrEditor.createVal.apply(i));
-						muProgMem.setInstruction(address, newInstr);
-						item.setText(column, instrEditor.getVal.apply(newInstr).toString());
-						updateItemColor(item, address);
-						machineChanged();
+			InstructionPropertyEditor instrPropEditor = instructionEditors.get(column);
+			if (instrPropEditor != null)
+				if (instrPropEditor.values == null) {
+					MicroInstruction instr = muProgMem.getInstruction(address);
+					Text text = new Text(cursor, SWT.NONE);
+					text.setText(instrPropEditor.getVal.apply(instr).toString());
+					text.selectAll();
+					text.addListener(SWT.FocusOut, e2 -> {
+						if (instrPropEditor.checkVal.test(text.getText()))
+							updateInstr(address, column, item, text.getText());
 						text.dispose();
-					} catch (IllegalArgumentException x) {
-					}
-				});
-				editor.setEditor(text);
-				text.setFocus();
-				break;
-			default:
-				CCombo combo;
-				combo = initEditInstrCombo(cursor, item, address);
-				editor.setEditor(combo);
-				combo.setListVisible(true);
-				combo.setFocus();
-			}
+					});
+					text.addListener(SWT.DefaultSelection, e2 -> {
+						if (instrPropEditor.checkVal.test(text.getText())) {
+							updateInstr(address, column, item, text.getText());
+							text.dispose();
+						}
+					});
+					editor.setEditor(text);
+					text.setFocus();
+				} else {
+					CCombo combo = new CCombo(cursor, SWT.NONE);
+					combo.setItems(instrPropEditor.values.stream().toArray(String[]::new));
+					combo.select(instrPropEditor.values.indexOf(item.getText(column)));
+					combo.addListener(SWT.FocusOut, e2 -> {
+						if (instrPropEditor.checkVal.test(combo.getText()))
+							updateInstr(address, column, item, combo.getText());
+						combo.dispose();
+					});
+					combo.addListener(SWT.DefaultSelection, e2 -> {
+						if (instrPropEditor.checkVal.test(combo.getText())) {
+							updateInstr(address, column, item, combo.getText());
+							combo.dispose();
+						}
+					});
+					editor.setEditor(combo);
+					combo.setListVisible(true);
+					combo.setFocus();
+				}
 		};
 		cursor.addListener(SWT.DefaultSelection, editListener);
 		cursor.addListener(SWT.MouseDown, editListener);
+
 	}
 
 	private void updateItemColor(int address) {
@@ -394,68 +316,18 @@ public class MicroinstructionsComposite extends Composite {
 	}
 
 	private boolean isInstrColumnUnchanged(MicroInstruction instr, int column) {
-		Function<MicroInstruction, ?> getVal;
-		switch (column) {
-		case 0:
+		if (column == 0)
 			return true;
-		case 2:
-		case 4:
-		case 8:
-		case 10:
-		case 15:
-		case 21:
-			getVal = instructionNBitsUIntEditors.get(column).getVal;
-			break;
-		default:
-			getVal = instructionEnumEditors.get(column).getVal;
-			break;
-		}
+		Function<MicroInstruction, ?> getVal = instructionEditors.get(column).getVal;
 		return getVal.apply(instr).equals(getVal.apply(MicroInstruction.DEFAULT));
 	}
 
-	private <E extends Enum<E>> CCombo initEditInstrCombo(TableCursor cursor, TableItem item, int address) {
+	private void updateInstr(int address, int column, TableItem item, String newVal) {
 		MicroInstruction instr = muProgMem.getInstruction(address);
-		@SuppressWarnings("unchecked")
-		InstructionEditorE<E> editor = (InstructionEditorE<E>) instructionEnumEditors.get(cursor.getColumn());
-		return initShortLivedEnumCCombo(cursor, editor.getVal.apply(instr),
-				updateInstr(address, cursor.getColumn(), item, editor.setVal));
-	}
-
-	private <E extends Enum<E>> Consumer<E> updateInstr(int address, int column, TableItem item,
-			BiFunction<MicroInstruction, E, MicroInstruction> setVal) {
-		return e -> {
-			MicroInstruction instr = muProgMem.getInstruction(address);
-			muProgMem.setInstruction(address, setVal.apply(instr, e));
-			updateItemColor(item, address);
-			item.setText(column, e.toString());
-			machineChanged();
-		};
-	}
-
-	private <E extends Enum<E>> CCombo initShortLivedEnumCCombo(TableCursor cursor, E currentVal, Consumer<E> setVal) {
-		E[] values = currentVal.getDeclaringClass().getEnumConstants();
-		CCombo combo = new CCombo(cursor, SWT.NONE);
-		combo.setItems(Arrays.stream(values).map(Enum::toString).toArray(String[]::new));
-		combo.select(currentVal.ordinal());
-		combo.addListener(SWT.FocusOut, e2 -> {
-			String text = combo.getText();
-			for (E e : values)
-				if (e.toString().equals(text)) {
-					setVal.accept(e);
-					break;
-				}
-			combo.dispose();
-		});
-		combo.addListener(SWT.DefaultSelection, e2 -> {
-			String text = combo.getText();
-			for (E e : values)
-				if (e.toString().equals(text)) {
-					setVal.accept(e);
-					combo.dispose();
-					break;
-				}
-		});
-		return combo;
+		muProgMem.setInstruction(address, instructionEditors.get(column).setVal.apply(instr, newVal));
+		updateItemColor(item, address);
+		item.setText(column, newVal);
+		machineChanged();
 	}
 
 	private void executeNextN(int n) {
